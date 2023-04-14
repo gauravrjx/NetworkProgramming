38f149eedbc771e994d9f0af5a3b0cecba692cf8
Gaurav <jr.gaurav2015@gmail.com>
2023-04-14 18:36:15 +0545

    add server

---
+++ NetworkProgramming/BasicClientServer/Server.java
@@ -0,0 +1,24 @@
+package BasicClientServer;
+import java.io.*;
+import java.net.*;
+
+public class Server{
+    public static void main(String[] args) throws IOException {
+        ServerSocket ss = new ServerSocket(9000);
+        System.out.println("Waiting for connections....");
+        Socket cs = ss.accept();
+        System.out.println("connected...");
+
+        // read the message
+        InputStreamReader in = new InputStreamReader(cs.getInputStream());
+        BufferedReader input = new BufferedReader(in);
+        String message = input.readLine();
+        System.out.println("client: " + message);
+
+        // send response
+        PrintWriter out = new PrintWriter(cs.getOutputStream(), true);
+        out.println("you said, "+ "'"+message+"'");
+
+        ss.close();
+    }
+}
