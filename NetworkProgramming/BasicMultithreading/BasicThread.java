c4fe9ae7e753f128c87b15df85d878bd48c9732b
Gaurav <jr.gaurav2015@gmail.com>
2023-04-15 19:11:07 +0545

    add more programs

---
+++ NetworkProgramming/BasicMultithreading/BasicThread.java
@@ -0,0 +1,14 @@
+package BasicMultithreading;
+
+public class BasicThread extends Thread{
+    public String threadNo;
+    public BasicThread(String i) {
+        this.threadNo = i;
+    }
+
+    @Override
+    public void run() {
+        // TODO Auto-generated method stub
+        System.out.println("running thread: "+ threadNo);
+    }
+}
