e6a89ca0bc6d79329f5d719325b0492703b62ed4
Gaurav <jr.gaurav2015@gmail.com>
2023-04-16 19:00:13 +0545

    update looger program

--- NetworkProgramming/ServerWithLog/Server.java
+++ NetworkProgramming/ServerWithLog/Server.java
@@ -5,28 +5,33 @@ import java.net.*;
 import java.util.logging.*;
 
 public class Server {
-    public static Logger logger = Logger.getLogger("requests");
+    public static Logger infoLog = Logger.getLogger("requests");
+    public static Logger errorLog = Logger.getLogger("errors");
 
     public static void main(String[] args) {
         ServerSocket ss=null;
         try {
             ss = new ServerSocket(9000);
         }catch(IOException e){
-            logger.log(Level.SEVERE, "could not start the server");
+            errorLog.log(Level.SEVERE, "could not start the server");
         }
 
-            logger.log(Level.INFO, "Waiting for connections....");
+            infoLog.info("Waiting for connections....");
+            /*
+             * Alternatively, it can also be written
+             * errorLog.log(Level.INFO, "Waiting for connections....")
+             */
         
             try{
             Socket cs;
             cs = ss.accept();
-            logger.log(Level.INFO, "CONNECTED");
+            infoLog.info("CONNECTED");
 
             // read the message
             InputStreamReader in = new InputStreamReader(cs.getInputStream());
             BufferedReader input = new BufferedReader(in);
             String message = input.readLine();
-            logger.log(Level.INFO, "client says: " + message);
+            infoLog.info("client says: " + message);
             // send response
             PrintWriter out = new PrintWriter(cs.getOutputStream(), true);
             out.println("you said, " + "'" + message + "'");
@@ -34,10 +39,10 @@ public class Server {
             ss.close();
         } catch (IOException e) {
             // TODO Auto-generated catch block
-            logger.log(Level.SEVERE, "Cannot establish the connection", e);
+            errorLog.log(Level.SEVERE, "Cannot establish the connection", e);
 
         } catch (Exception e) {
-            logger.log(Level.SEVERE, "Something went wrong " + e.getMessage(), e);
+            errorLog.log(Level.SEVERE, "Something went wrong " + e.getMessage(), e);
         }
     }
 }
