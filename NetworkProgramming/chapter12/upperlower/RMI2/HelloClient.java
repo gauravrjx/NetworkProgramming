c4ced35c1aa4d3c04a41f44bd0064d146d435889
Gaurav <jr.gaurav2015@gmail.com>
2023-05-20 08:13:48 +0545

    add program

---
+++ NetworkProgramming/chapter12/upperlower/RMI2/HelloClient.java
@@ -0,0 +1,20 @@
+package chapter12.upperlower.RMI2;
+
+import java.rmi.*;
+import java.io.*;
+
+public class HelloClient
+{
+  public static void main (String[] argv) 
+  {
+    try 
+    {
+      HelloInterface hello = (HelloInterface) Naming.lookup("//localhost/Hello");
+      System.out.println(hello.say());
+    }
+    catch (Exception e) 
+    {
+      System.out.println ("HelloClient exception: " + e);
+    }
+  }
+}
