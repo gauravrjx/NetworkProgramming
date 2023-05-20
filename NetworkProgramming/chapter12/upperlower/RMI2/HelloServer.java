515f6a6b22b4e4ac41589d95b2d2c67e6e47a3be
Gaurav <jr.gaurav2015@gmail.com>
2023-05-20 08:16:14 +0545

    add program

---
+++ NetworkProgramming/chapter12/upperlower/RMI2/HelloServer.java
@@ -0,0 +1,21 @@
+package chapter12.upperlower.RMI2;
+
+import java.io.*;
+import java.rmi.*;
+
+public class HelloServer
+{
+  public static void main (String[] argv) 
+  {
+    try 
+    {
+      Hello robj = new Hello ("Hello, world!");
+      Naming.rebind ("Hello", robj);
+      System.out.println ("Hello Server is ready.");
+    } 
+    catch (Exception e) 
+    {
+      System.out.println ("Hello Server failed: " + e);
+    }
+  }
+}
