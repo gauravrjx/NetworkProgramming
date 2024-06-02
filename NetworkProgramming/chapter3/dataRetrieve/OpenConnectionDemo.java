a45d01d7de145ee75450b32e79702098bfe9b434
Gaurav Jaiswal <jr.gaurav2015@gmail.com>
2024-06-02 15:30:40 +0545

    update openconnection

--- NetworkProgramming/chapter3/dataRetrieve/OpenConnectionDemo.java
+++ NetworkProgramming/chapter3/dataRetrieve/OpenConnectionDemo.java
@@ -6,13 +6,15 @@ import java.net.*;
 public class OpenConnectionDemo {
 	public static void main(String[] args) {
 		try {
-			URL url = new URL("https://www.youtube.com/");
+			URL url = new URL("https://dummyjson.com/recipes/1");
 			URLConnection urlcon = url.openConnection();
 			InputStream stream = urlcon.getInputStream();
 			int i;
 			while ((i = stream.read()) != -1) {
 				System.out.print((char) i);
 			}
+			System.out.println("\nURL Header => Content Type: " + urlcon.getContentType());
+			System.out.println("URL Header => Content Length : " + urlcon.getContentLength());
 		} catch (IOException e) {
 			System.out.println(e);
 		}
