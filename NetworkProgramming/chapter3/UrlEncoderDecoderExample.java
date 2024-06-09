7cda75d5934ad9910c9fc9f5888843b2cf58ba78
Gaurav Jaiswal <jr.gaurav2015@gmail.com>
2024-06-09 12:15:29 +0545

    update url encode

--- NetworkProgramming/chapter3/UrlEncoderDecoderExample.java
+++ NetworkProgramming/chapter3/UrlEncoderDecoderExample.java
@@ -7,19 +7,19 @@ public class UrlEncoderDecoderExample {
    public static void main(String[] args) {
       try {
          // URL to be encoded
-         String url = "https://www.example.com/search?q=query string";
+         String url = "https://www.example.com/about us/search?q=query string";
 
          // Encode the URL
          String encodedUrl = URLEncoder.encode(url, "UTF-8");
 
          // Print the encoded URL
-         System.out.println(encodedUrl); // Output: https%3A%2F%2Fwww.example.com%2Fsearch%3Fq%3Dquery+string
+         System.out.println(encodedUrl);
 
          // Decode the URL
          String decodedUrl = URLDecoder.decode(encodedUrl, "UTF-8");
 
          // Print the decoded URL
-         System.out.println(decodedUrl); // Output: https://www.example.com/search?q=query string
+         System.out.println(decodedUrl);
 
       } catch (Exception e) {
          e.printStackTrace();
